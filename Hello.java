6e34ad2de77ecd32fcdc1b58576b62c0729e4a27
Kitamura Tsutomu <tsu_kitamura@worksid.co.jp>
2021-06-14 16:45:13 +0900

    fixed.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String[] args){
-		System.out.println("Hello, Japan.!!");
+		for(int i=0;i<3;i++) {
+			System.out.println("Hello, Japan.!!");
+		}
 	}
 }
